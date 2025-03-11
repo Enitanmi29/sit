93a3f579091feefeab26a353b468281f99964bb5
tshaba29 <abidakuntaiwoenitan@gmail.com>
2025-03-11 09:29:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var guestList = ["Taiwo", "Kenny", "Micheal", "Boniface", "Marvelous", "Stephen", "Matron", "Iya Atunde", "Teelop", "Racheal", "Ogunfunwa"];
+// function (name){
+//     if(name === guestList){
+//        alert("You're welcome" + name);
+//        console.log(name);
+//     } else {
+//        alert("You're not on the list, please go ahead and register");
+//     }
+// }
